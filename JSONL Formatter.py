eb63d3ef405c63c81522a8bad2d61c764d059593
Evelyn Jennings <recovjohn@gmail.com>
2023-02-21 02:53:15 -0500

    Update JSONL Formatter.py

--- JSONL Formatter.py
+++ JSONL Formatter.py
@@ -1,4 +1,4 @@
-#Copyright 2018 Dillon Jennings
+#Copyright 2018 Evelyn Jennings
 
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
